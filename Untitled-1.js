f5a4abf26b851ac61b75d549384a1fe06f7868de
romejg <justineguillermo00@gmail.com>
2024-04-24 21:44:29 +0800

    add new codes for demo

---
+++ Untitled-1.js
@@ -0,0 +1,31 @@
+// js operators
+
+{
+    var gX = "hello"
+    let x = "string 1 "
+    y = "string 2"
+    z = x + y // will output "string 1 string 2"
+    console.log(z)
+}
+{
+    //multiply two numbers then output the result
+    let x = 2
+    y = 3
+    z = x * y
+    console.log("output of x * y is " + z)
+}
+{
+    //divide two numbers then output the result
+    let x = 10
+    y = 4
+    z = x / y
+    console.log("output of x / y is " + z)
+}
+{
+    //modulus two numbers then output the result
+    let x = 10
+    y = 4
+    z = x % y
+    console.log("output of x % y is " + z)
+    console.log(gX)
+}
