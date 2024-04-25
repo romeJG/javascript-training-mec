9bfe2d841fe19c61a3bed889ec2779cba38cf6be
romeJG <58583590+romeJG@users.noreply.github.com>
2024-04-25 13:40:53 +0800

    opperations in inputs

---
+++ if-else/app.js
@@ -0,0 +1,16 @@
+function getAnswer(operation) {
+    let var1 = document.getElementById("var1").value;
+    let var2 = document.getElementById("var2").value;
+    let result;
+    if (operation === "add") {
+        result = parseFloat(var1) + parseFloat(var2);
+    } else if (operation === "subtract") {
+        result = parseFloat(var1) - parseFloat(var2);
+    } else if (operation === "multiply") {
+        result = parseFloat(var1) * parseFloat(var2);
+    } else if (operation === "divide") {
+        result = parseFloat(var1) / parseFloat(var2);
+    }
+    document.getElementById("answer").innerHTML = result;
+}
+
