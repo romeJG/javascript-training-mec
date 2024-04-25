c53369ef88e43470a8f82998d86e49c8cfc47311
romeJG <58583590+romeJG@users.noreply.github.com>
2024-04-25 15:41:07 +0800

    answerkey area of shapes

--- if-else/app.js
+++ if-else/app.js
@@ -2,14 +2,16 @@ function getAnswer(operation) {
     let var1 = document.getElementById("var1").value;
     let var2 = document.getElementById("var2").value;
     let result;
-    if (operation === "add") {
-        result = parseFloat(var1) + parseFloat(var2);
-    } else if (operation === "subtract") {
-        result = parseFloat(var1) - parseFloat(var2);
-    } else if (operation === "multiply") {
+    if (operation === "square") {
+        if (var2) { //checks if var 2 has a value
+            result = "Please enter only one number"
+        } else {
+            result = parseFloat(var1) ** 2
+        }
+    } else if (operation === "rectangle") {
         result = parseFloat(var1) * parseFloat(var2);
-    } else if (operation === "divide") {
-        result = parseFloat(var1) / parseFloat(var2);
+    } else if (operation === "triangle") {
+        result = 0.5 * parseFloat(var1) * parseFloat(var2);
     } else {
         result = "unknown shape"
     }
